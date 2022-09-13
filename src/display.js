cb8dab0e5edeb77f6e0bbbf6ad56aea5b081bef3
Ervin <evgnz24@gmail.com>
2022-09-13 16:02:19 -0500

    Implemented dark mode (not fully)

--- src/display.js
+++ src/display.js
@@ -1,12 +1,26 @@
 export class DisplayUI {
-  showWinner(data) {
-    console.log(data);
-  }
-
-  containerAdd() {
+  addHome() {
+    // main Container El
     const containerEl = document.createElement('div');
     containerEl.classList.add('container');
-
     document.body.appendChild(containerEl);
+
+    // First child
+    const headerEl = document.createElement('header');
+    headerEl.classList.add('main', 'header');
+    containerEl.appendChild(headerEl);
+
+    // ^logo
+    const logoEl = document.createElement('div');
+    logoEl.textContent = 'TODO';
+    logoEl.classList.add('logo', 'top');
+    headerEl.appendChild(logoEl);
+
+    // ^options
+
+    // Second Child
+    const mainContentEl = document.createElement('div');
+    mainContentEl.classList.add('main', 'main-content');
+    containerEl.appendChild(mainContentEl);
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import '../assets/style.css';
 import { DisplayUI } from './display.js';
+import { compareAsc, format, addDays, startOfDay, getDate } from 'date-fns';
+
+// This is for toggling dark and light mode
+// const root = document.documentElement;
+const date = new Date();
+const formattedDate = format(date, 'MM/dd/yyyy');
+const today = getDate(date);
 
 const displayUI = new DisplayUI();
-displayUI.showWinner('Hello  World');
-displayUI.containerAdd();
+displayUI.addHome();
