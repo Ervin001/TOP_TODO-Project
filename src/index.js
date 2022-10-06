a6e1afd27acb351c504aaa1a7f20cd99eb214a41
Ervin <evgnz24@gmail.com>
2022-10-05 19:35:24 -0500

    last

--- src/index.js
+++ src/index.js
@@ -9,25 +9,16 @@ const formattedDate = format(date, 'MM/dd/yyyy');
 const today = getDate(date);
 
 const displayUI = new DisplayUI();
-displayUI.addHome();
+displayUI.homePage();
 
 // Object created by user in[ut will go here to add to DOM]
-displayUI.addTodoItem('Get Milk', formattedDate);
-displayUI.addTodoItem('Get Milk', formattedDate);
-displayUI.addTodoItem('Get Milk', formattedDate);
+// displayUI.addTodoItem('Get Milk', formattedDate);
+// displayUI.addTodoItem('Get Milk', formattedDate);
+// displayUI.addTodoItem('Get Milk', formattedDate);
 const newTaskBtn = document.querySelector('.todo-button');
 
 newTaskBtn.addEventListener('click', (e) => {
   displayUI.showForm();
-  const formValues = document.querySelector('.bottom-container');
-
-  formValues.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const inputValInput = document.getElementById('inpt');
-    let inputVal = inputValInput.value;
-
-    displayUI.addTodoItem(inputVal, formattedDate);
-    displayUI.removeFormContainer();
-    inputVal = '';
-  });
+  // const formValues = document.querySelector('.bottom-container');
+  console.log('Test');
 });
