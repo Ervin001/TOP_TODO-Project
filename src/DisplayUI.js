7dff861edc6853bbc64e56ab17eb810ca920c493
Ervin <evgnz24@gmail.com>
2022-10-12 19:22:27 -0500

    added more container to the main container

--- src/DisplayUI.js
+++ src/DisplayUI.js
@@ -2,17 +2,40 @@ export default class Display {
   loadHome() {
     // main container
     const containerEl = document.createElement('div');
-    containerEl.textContent = 'Hello World';
-    containerEl.classList.add('container', 'grid', 'grid--3');
+    containerEl.classList.add('container');
     document.body.appendChild(containerEl);
 
-    // sidebar container
+    // logo container
+    const logoContainerEl = document.createElement('div');
+    logoContainerEl.classList.add('logo', 'grid-item');
+    logoContainerEl.textContent = 'Logo';
+    containerEl.appendChild(logoContainerEl);
 
-    // header view
-    // todo item
+    // header container
+    const headerEl = document.createElement('div');
+    headerEl.classList.add('header', 'grid-item');
+    containerEl.appendChild(headerEl);
+    // prettier-ignore
+    // Header first child
+    const firstHeaderChild = document.createElement('div');
+    firstHeaderChild.classList.add('header-child', 'first-child');
+    firstHeaderChild.textContent = 'First Child';
+    headerEl.appendChild(firstHeaderChild);
+    // sidebar container
+    const sidebarContainerEl = document.createElement('aside');
+    sidebarContainerEl.classList.add('sidebar', 'grid-item');
+    sidebarContainerEl.textContent = 'Side Bar';
+    containerEl.appendChild(sidebarContainerEl);
+
+    // main content
+    const mainContentEl = document.createElement('div');
+    mainContentEl.classList.add('main-content', 'grid-item');
+    mainContentEl.textContent = 'Main Content';
+    containerEl.appendChild(mainContentEl);
   }
 
   addTodo(data) {}
+  todoItems(array) {}
   deleteTodo(data) {}
   showForm() {}
   hideForm() {}
