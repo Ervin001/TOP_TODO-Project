bf16abca85d7da27a8f7ac2bddaf97781e993559
Ervin <evgnz24@gmail.com>
2022-10-13 11:39:42 -0500

    reworked the whole grid  changed the layout of the todo project

--- src/DisplayUI.js
+++ src/DisplayUI.js
@@ -1,3 +1,4 @@
+import { format } from 'date-fns';
 export default class Display {
   loadHome() {
     // main container
@@ -5,38 +6,28 @@ export default class Display {
     containerEl.classList.add('container');
     document.body.appendChild(containerEl);
 
-    // logo container
-    const logoContainerEl = document.createElement('div');
-    logoContainerEl.classList.add('logo', 'grid-item');
-    containerEl.appendChild(logoContainerEl);
-    // Logo text container
-    const logoTextEl = document.createElement('h1');
-    logoTextEl.classList.add('logo-text');
-    logoTextEl.textContent = 'Todo';
-    logoContainerEl.appendChild(logoTextEl);
+    // Header container
+    const headerContainerEl = document.createElement('div');
+    headerContainerEl.classList.add('grid-item', 'header');
+    containerEl.appendChild(headerContainerEl);
+    // Hamburger Menu
+    const hamburgerMenuEl = document.createElement('div');
+    hamburgerMenuEl.classList.add('hamburger-menu', 'header-child');
+    headerContainerEl.appendChild(hamburgerMenuEl);
+    // Add todos button
+    const addButtonEl = document.createElement('div');
+    addButtonEl.classList.add('add-btn', 'header-child');
+    headerContainerEl.appendChild(addButtonEl);
 
-    // header container
-    const headerEl = document.createElement('div');
-    headerEl.classList.add('header', 'grid-item');
-    containerEl.appendChild(headerEl);
-    // Header first child
-    const firstHeaderChild = document.createElement('div');
-    firstHeaderChild.classList.add('header-child', 'first-child');
-    headerEl.appendChild(firstHeaderChild);
-    // Header second child
-    const secondHeaderChild = document.createElement('div');
-    secondHeaderChild.classList.add('header-child', 'second-child');
-    headerEl.appendChild(secondHeaderChild);
-    // sidebar container
+    // Sidebar container
     const sidebarContainerEl = document.createElement('aside');
-    sidebarContainerEl.classList.add('sidebar', 'grid-item');
-    sidebarContainerEl.textContent = 'Side Bar';
+    sidebarContainerEl.classList.add('grid-item', 'sidebar');
     containerEl.appendChild(sidebarContainerEl);
 
-    // main content
-    const mainContentEl = document.createElement('div');
-    mainContentEl.classList.add('main-content', 'grid-item');
-    containerEl.appendChild(mainContentEl);
+    // _Todo container
+    const todoContainerEl = document.createElement('div');
+    todoContainerEl.classList.add('grid-item', 'todo-container');
+    containerEl.appendChild(todoContainerEl);
   }
 
   addTodo(data) {}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './styles.css';
 import Display from './displayUI.js';
+import { getDate, format } from 'date-fns';
 
 const display = new Display();
 
