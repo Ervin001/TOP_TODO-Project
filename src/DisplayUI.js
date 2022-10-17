23936163167d0d60d5b32da8b83d6b8b05844682
Ervin <evgnz24@gmail.com>
2022-10-17 13:18:09 -0500

    fixed svg's in sidebar, now there is text as well

--- src/DisplayUI.js
+++ src/DisplayUI.js
@@ -47,6 +47,7 @@ export default class Display {
     addButtonIconText.classList.add('btn-txt');
     addButtonIconText.textContent = 'Add Todo';
     addButtonEl.appendChild(addButtonIconText);
+
     // Sidebar container
     const sidebarContainerEl = document.createElement('aside');
     sidebarContainerEl.classList.add('grid-item', 'sidebar');
@@ -55,21 +56,56 @@ export default class Display {
     const svgContainerEl = document.createElement('div');
     svgContainerEl.classList.add('svg-container');
     sidebarContainerEl.appendChild(svgContainerEl);
+    // Inbox svg container
+    const inboxContainer = document.createElement('div');
+    inboxContainer.classList.add('inbox-container', 'sidebar-svg-container');
+    svgContainerEl.appendChild(inboxContainer);
     // Inbox svg
     const inboxSvg = new Image();
     inboxSvg.src = FileTray;
     inboxSvg.classList.add('inbox-svg', 'sidebar-svg');
-    svgContainerEl.appendChild(inboxSvg);
+    inboxContainer.appendChild(inboxSvg);
+    // inbox text
+    const inboxText = document.createElement('p');
+    inboxText.classList.add('inbox-svg-text', 'svg-text');
+    inboxText.textContent = 'Inbox';
+    inboxContainer.appendChild(inboxText);
+    // Today svg container
+    const todayContainer = document.createElement('div');
+    todayContainer.classList.add('today-container', 'sidebar-svg-container');
+    svgContainerEl.appendChild(todayContainer);
     // Today svg
     const todaySvg = new Image();
     todaySvg.src = Today;
     todaySvg.classList.add('today-svg', 'sidebar-svg');
-    svgContainerEl.appendChild(todaySvg);
+    todayContainer.appendChild(todaySvg);
+    // Today text
+    const todayText = document.createElement('p');
+    todayText.classList.add('today-svg-text', 'svg-text');
+    todayText.textContent = 'Today';
+    todayContainer.appendChild(todayText);
+    // collection svg container
+    const collectionContainer = document.createElement('div');
+    collectionContainer.classList.add(
+      'collection-container',
+      'sidebar-svg-container'
+    );
+    svgContainerEl.appendChild(collectionContainer);
     // collections svg
     const albumsSvg = new Image();
     albumsSvg.src = Albums;
     albumsSvg.classList.add('collections-svg', 'sidebar-svg');
-    svgContainerEl.appendChild(albumsSvg);
+    collectionContainer.appendChild(albumsSvg);
+    // Collection Text
+    const collectionText = document.createElement('p');
+    collectionText.classList.add('collection-svg-text', 'svg-text');
+    collectionText.textContent = 'Collections';
+    collectionContainer.appendChild(collectionText);
+    // light/dark mode
+    const lightDarkSvg = document.createElement('div');
+    lightDarkSvg.src = Contrast;
+    lightDarkSvg.classList.add('light-dark-mode', 'sidebar-svg');
+    svgContainerEl.appendChild(lightDarkSvg);
 
     // _Todo container
     const todoContainerEl = document.createElement('div');
